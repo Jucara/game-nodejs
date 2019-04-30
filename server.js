ae300c9d475eab2c75b0f819d7d2219a674c97b0
Monticelli Romain <rmm.monticelli@gmail.com>
2019-04-30 16:37:09 +0200

    basic socket.io

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-
-console.log(100000);
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
+let app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+let game = require('./game.js')
 
 http.listen(4000, function(){
-  console.log('listening on *:4000');
+  console.log('listening on: 4000')
 });
+
+
+io.on('connection', function(socket){
+  console.log('Client connected');
+  if (players.length >= 2 )
+    return;
+  players.push(socket);
+  socket.on("Press button", function(data){
+    console.log('Received button press', data)
+    if (players.indexOf(socket) == turn){
+      socket.emit('message', 'Nice, you played a turn');
+      turn = (turn+1)%2
+    } else {
+      socket.emit('message', "It's not your turn, fool!");
+    }
+  })
+})
+
+let players = [];
+let turn = 0;
--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import Routes from './routes';
+import io from 'socket.io-client';
 
 export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      socket: io.connect('http://localhost:4000')
+    }
+  }
   render() {
     return (
       <div className="App">
